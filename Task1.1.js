079b61a8bb45b06b3af6a16c2cdc079bb187afcf
Shenavieve <98396663+Shenavieve@users.noreply.github.com>
2022-03-02 16:49:23 +0200

    Update Task1.1.js

--- Task1.1.js
+++ Task1.1.js
@@ -1,7 +1,7 @@
 function multiples (num) {
-  var sum = 0;
-  for (var i =1; i < num ; i++) {
-    if ((i% 3 == 0) || (i% 5== 0)){
+  let sum = 0;
+  for (let i =1; i < num ; i++) {
+    if ((i% 3 === 0) || (i% 5=== 0)){
       sum+= i;
     }
   }
